251425fb8f59785f17cf586cd05fa246aa95d714
sundarasekar <sundarasekar01@gmail.com>
2024-02-03 03:44:53 +0530

    basics

--- index.js
+++ index.js
@@ -1,1 +1,96 @@
+/* ------------------------------------- Introduction ------------------------------------------- */
 
+
+// JavaScript = scripting language for webpages
+//              can be used for both client-side and server-side developments.
+
+// This is a command
+
+/*
+   this 
+   is 
+   a 
+   command
+*/
+
+// console.log('print statement');
+// window.alert('how are you');
+
+// document.getElementById('abc').textContent = 'hi';
+
+
+
+
+
+/* ------------------------------------- Variables ------------------------------------------- */
+
+
+// variable = A container that stores a value.
+//                   Behaves as if it were the value it contains.
+
+// let fullName = "Bro Code";
+// let age = 25;
+// let isStudent = false;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} years old`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
+
+
+
+/* -------------------------------- Arithmetic Operators ------------------------------------------- */
+
+
+
+// arithmetic operators = operands (values, variables, etc.)
+//                                         operators (+ - * /)
+//                                         ex. 11 = x + 5;
+
+// let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let extraStudents = students % 3;
+
+// console.log(students);
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+//students++;
+//students--;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+
+// let result = (2+8) * 3+1 - 2**3 ; 
+// console.log(result);
+
+/*  
+Explanation :
+
+=> 10 * 3+1 - 8
+=> 30 + 1 - 8
+=> 31-8 
+=> 23 
+*/          
+
+
+
+
+/* -------------------------------- Arithmetic Operators ------------------------------------------- */
