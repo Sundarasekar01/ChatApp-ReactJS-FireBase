3886053da1919333e780133345b4b0cac06fddaf
sundarasekar <sundarasekar01@gmail.com>
2024-02-23 12:22:41 +0530

    .

--- index.js
+++ index.js
@@ -216,25 +216,25 @@ Explanation :
 // Math = built-in object that provides a
 //              collection of properties and methods
 
-// let x = 3;
-// let y = 2;
-// let z = 1;
+let x = 3;
+let y = 2;
+let z = 1;
 
 //console.log(Math.PI);
 //console.log(Math.E);
 
-//z = Math.round(x);
-//z = Math.floor(x);
-//z = Math.ceil(x);
-//z = Math.trunc(x);
-//z = Math.pow(x, y);
-//z = Math.sqrt(x);
-//z = Math.log(x);
-//z = Math.sin(x);
-//z = Math.cos(x);
-//z = Math.tan(x);
-//z = Math.abs(x);
-//z = Math.sign(x);
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x);
+z = Math.pow(x, y);
+z = Math.sqrt(x);
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.abs(x);
+z = Math.sign(x);
 
 
 // let max = Math.max(x, y, z);
@@ -252,10 +252,10 @@ Explanation :
 
 // RANDOM NUMBER GENERATOR
 
-// const NUM1 = document.getElementById('num1');
-// const NUM2 = document.getElementById('num2');
-// const NUM3 = document.getElementById('num3');
-// const GENERATE = document.getElementById('btn');
+const NUM1 = document.getElementById('num1');
+const NUM2 = document.getElementById('num2');
+const NUM3 = document.getElementById('num3');
+const GENERATE = document.getElementById('btn');
 
 // const MIN = 23;
 // const MAX = 45;
